ef9d22e2b90df4bdfc3e4bfecfe18d9dd7e6b0ba
Nick Booker <nick.booker@opusvl.com>
2015-08-07 15:23:15 +0100

    B3897 : Add module sale_printout_split_sku

---
+++ sale_printout_split_sku/__openerp__.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+# Split SKU from Description on Sale Order printout
+# Copyright (C) 2015 OpusVL (<http://opusvl.com/>)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Split SKU from Description on Sale Order printout',
+    'version': '0.1',
+    'author': 'OpusVL',
+    'website': 'http://opusvl.com/',
+    'summary': 'Split SKU from Description on Sale Order printout',
+    
+    'category': 'Sales',
+    
+    'description': """Split SKU from Description on Sale Order printout.
+""",
+    'images': [
+    ],
+    'depends': [
+        'sale',
+        'base',
+    ],
+    'data': [
+        'reports/sale_order.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ sale_printout_split_sku/sale_order.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+# Split SKU from Description on Sale Order printout
+# Copyright (C) 2015 OpusVL (<http://opusvl.com/>)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    description_without_sku = fields.Char(
+        compute="_description_without_sku_compute",
+        readonly=True,
+    )
+
+    product_sku = fields.Char(
+        related=['product_tmpl_id', 'default_code'],
+        readonly=True,
+    )
+
+    @api.depends('name', 'product_sku')
+    @api.one
+    def _description_without_sku_compute(self):
+        if self.product_sku and self.name:
+            sku_prefix = '[%s] ' % (self.product_sku,)
+            self.description_without_sku = self.name.replace(sku_prefix, '')
+        else:
+            self.description_without_sku = self.name
+        
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
