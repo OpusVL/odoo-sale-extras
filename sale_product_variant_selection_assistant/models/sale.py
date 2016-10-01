7b8cf72e942510b5652a90e20ba8d16dbe32dcca
Nick Booker <nick.booker@opusvl.com>
2016-10-01 14:53:12 +0100

    Add docstrings to the sale order models.

--- sale_product_variant_selection_assistant/models/sale.py
+++ sale_product_variant_selection_assistant/models/sale.py
@@ -43,21 +43,29 @@ class SaleOrderLine(models.Model):
     
     @api.returns('product.attribute')
     def _assistant_attributes(self):
+        """Return the attributes available on the chosen product template.
+        """
         return self.variant_assistant_product_template_id.attribute_line_ids.mapped('attribute_id')
 
     
     @api.returns('product.attribute.value')
     def _assistant_available_values(self, attribute):
+        """Return the values available for the given attribute on the chosen product template.
+        """
         template = self.variant_assistant_product_template_id
         return attribute.value_ids.filtered(lambda v: v in template.attribute_line_ids.mapped('value_ids'))
 
 
     def _chosen_values(self):
+        """Return the values the user has chosen.
+        """
         choices = self.variant_assistant_attribute_choice_ids.filtered(lambda c: c.value_id)
         return choices.mapped('value_id')
         
 
     def _assistant_resolve_variant(self):
+        """Return a unique variant matching chosen template and attribute choices.
+        """
         empty_variants = self.env['product.product']
         template = self.variant_assistant_product_template_id
         if not template:
@@ -74,6 +82,8 @@ class SaleOrderLine(models.Model):
     
     @api.onchange('variant_assistant_product_template_id')
     def _onchange_variant_assistant_product_template_id(self):
+        """Populate the choices table with options appropriate to chosen template.
+        """
         changes = [(2, i, False) for i in self.variant_assistant_attribute_choice_ids.ids]
         attrs = self._assistant_attributes()
         for attr in attrs:
@@ -84,8 +94,11 @@ class SaleOrderLine(models.Model):
             changes.append((0, False, choice_data))
         self.update({'variant_assistant_attribute_choice_ids': changes})
 
+
     @api.onchange('variant_assistant_attribute_choice_ids')
     def _onchange_variant_assistant_attribute_choice_ids(self):
+        """Resolve the variant when updates to choices have been completed.
+        """
         self.product_id = self._assistant_resolve_variant()
 
 
@@ -97,11 +110,13 @@ class SaleOrderLineAssistantAttributeChoice(models.Model):
         string='Sale Order Line',
         comodel_name='sale.order.line',
     )
+    
     product_template_id = fields.Many2one(
         string='Product Template',
         readonly=True,
         related='sale_order_line_id.variant_assistant_product_template_id',
     )
+    
     attribute_id = fields.Many2one(
         string='Attribute',
         comodel_name='product.attribute',
@@ -113,9 +128,19 @@ class SaleOrderLineAssistantAttributeChoice(models.Model):
         comodel_name='product.attribute.value',
     )
 
+    
     @api.one
     @api.constrains('product_template_id', 'attribute_id', 'value_id')
     def _check_attributes(self):
+        """Make sure the attributes choices list is sane.
+
+        This is largely to catch errors let through by any holes there
+        might be in the domain filtering.  In general, if these are producing
+        errors to the user, the domain filtering probably needs adjusting.
+
+        It also guards against users trying to force garbage through, and
+        errors in records altered via API calls and tests.
+        """
         if self.attribute_id not in self.product_template_id.attribute_line_ids.mapped('attribute_id'):
             raise ValidationError('Attribute must match product template')
         if self.value_id.attribute_id != self.attribute_id:
