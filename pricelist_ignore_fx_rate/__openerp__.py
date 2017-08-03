c29f9b5f9d5ce1b01d2ec85cb4bd4f9ff9c4554c
Peter Alabaster <peter.alabaster@opusvl.com>
2017-08-03 11:43:44 +0100

    [ADD][T12740] Module to ignore FX conversion on pricelist
    Unfortunately we can't do this any cleaner, as the base Odoo func
    is a bag of shit

---
+++ pricelist_ignore_fx_rate/__openerp__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+# pricelist_ignore_fx_rate
+# Copyright (C) 2017 OpusVL (<http://opusvl.com/>)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Pricelist Ignore FX Rate',
+    'version': '0.1',
+    'author': 'OpusVL',
+    'website': 'http://opusvl.com/',
+    'summary': '',
+    'category': '',
+    'description': 'Ignore the currency exchange rate when using pricelists on sales orders. Note: Manual Pricelist calculations are still done!',
+    'images': [
+    ],
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/product_pricelist_view.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
